93d1c86e00185a5b6ca8aeef7fad843b6b631337
Ronald Record <github@ronrecord.com>
2021-03-29 13:15:36 -0700

    Handle zone commands in a single script

---
+++ api/zone_command.py
@@ -0,0 +1,63 @@
+import argparse
+import configparser
+import os.path
+from os import path
+
+config = configparser.ConfigParser()
+config.read('roonapi.ini')
+
+# Set to IP address of your Roon Core
+server = config['DEFAULT']['RoonCoreIP']
+# Name of the file that holds a Roon API token
+tokenfile = config['DEFAULT']['TokenFileName']
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--command", help="command selection")
+parser.add_argument("-z", "--zone", help="zone selection")
+args = parser.parse_args()
+
+if args.command:
+    zone_command = args.command
+else:
+    zone_command = "play"
+if args.zone:
+    target_zone = args.zone
+else:
+    target_zone = config['DEFAULT']['DefaultZone']
+
+from roonapi import RoonApi
+appinfo = {
+    "extension_id": "python_roon_test",
+    "display_name": "Python library for Roon",
+    "display_version": "1.0.0",
+    "publisher": "gregd",
+    "email": "mygreat@emailaddress.com",
+}
+
+# Can be None if you don't yet have a token
+if path.exists(tokenfile):
+    token = open(tokenfile).read()
+else:
+    token = "None"
+
+roonapi = RoonApi(appinfo, token, server)
+
+# get target zone output_id
+zones = roonapi.zones
+output_id = [
+    output["zone_id"]
+    for output in zones.values()
+    if target_zone in output["display_name"]
+][0]
+
+# Send the command to the specified zone
+if zone_command == "mute":
+    roonapi.mute(output_id, True)
+elif zone_command == "unmute":
+    roonapi.mute(output_id, False)
+else:
+    roonapi.playback_control(output_id, zone_command)
+
+# save the token for next time
+with open(tokenfile, "w") as f:
+    f.write(roonapi.token)
