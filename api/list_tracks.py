e27cdfbd410a29b731b39a71b46215956d123d02
Ronald Record <github@ronrecord.com>
2024-05-22 07:14:33 -0700

    Add tracks listing and album track play

---
+++ api/list_tracks.py
@@ -0,0 +1,91 @@
+import argparse
+import configparser
+from os import path
+import sys
+from roonapi import RoonApi
+
+config = configparser.ConfigParser()
+config.read('/usr/local/Roon/etc/roon_api.ini')
+
+# Set to IP address of your Roon Core
+server = config['DEFAULT']['RoonCoreIP']
+# Set to Port of your Roon Core
+port = config['DEFAULT']['RoonCorePort']
+# Name of the file that holds a Roon API token
+tokenfile = config['DEFAULT']['TokenFileName']
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-q", "--quiet", help="only output track titles")
+parser.add_argument("-t", "--track", help="track search term")
+parser.add_argument("-x", "--extrack", help="track exclude search term")
+parser.add_argument("-z", "--zone", help="zone selection")
+args = parser.parse_args()
+
+if args.quiet:
+    verbose = False
+else:
+    verbose = True
+if args.track:
+    tracksearch = args.track
+else:
+    tracksearch = "__all__"
+if args.extrack:
+    extracksearch = args.extrack
+else:
+    extracksearch = None
+if args.zone:
+    target_zone = args.zone
+else:
+    target_zone = config['DEFAULT']['DefaultZone']
+
+version = config['DEFAULT']['RoonCommandLineVersion']
+release = config['DEFAULT']['RoonCommandLineRelease']
+fullver = version + "-" + release
+
+appinfo = {
+    "extension_id": "roon_command_line",
+    "display_name": "Python library for Roon",
+    "display_version": fullver,
+    "publisher": "RoonCommandLine",
+    "email": "roon@ronrecord.com",
+    "website": "https://github.com/doctorfree/RoonCommandLine",
+}
+
+# Can be None if you don't yet have a token
+if path.exists(tokenfile):
+    token = open(tokenfile).read()
+else:
+    token = "None"
+
+roonapi = RoonApi(appinfo, token, server, port)
+
+# save the token for next time
+with open(tokenfile, "w") as f:
+    f.write(str(roonapi.token))
+
+# get target zone output_id
+outputs = roonapi.outputs
+
+output_id = None
+for (k, v) in outputs.items():
+    if target_zone in v["display_name"]:
+        output_id = k
+
+if output_id is None:
+    err = "No zone found matching " + target_zone
+    sys.exit(err)
+
+# List matching tracks
+tracks = roonapi.list_media(output_id, ["Library", "Tracks", tracksearch])
+if extracksearch is not None and tracks:
+    tracks = [tr for tr in tracks if extracksearch not in tr]
+if tracks:
+    if "Play Album" in tracks:
+        tracks.remove("Play Album")
+    found = tracks[0]
+    if verbose:
+        if tracksearch == "__all__":
+            print("\nTrack titles in Library:\n")
+        else:
+            print("\nTrack titles in Library matching", tracksearch, ":\n")
+    print(*tracks, sep="\n")
--- api/play_album_track.py
+++ api/play_album_track.py
@@ -15,31 +15,25 @@ port = config['DEFAULT']['RoonCorePort']
 tokenfile = config['DEFAULT']['TokenFileName']
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--album", help="album search term")
-parser.add_argument(
-    "-q",
-    "--quiet",
-    default=False,
-    action="store_true",
-    help="list albums without other output"
-)
 parser.add_argument("-t", "--track", help="track search term")
+parser.add_argument("-a", "--album", help="album search term")
+parser.add_argument("-X", "--extrack", help="track exclude search term")
 parser.add_argument("-x", "--exalbum", help="album exclude search term")
 parser.add_argument("-z", "--zone", help="zone selection")
 args = parser.parse_args()
 
+if args.track:
+    tracksearch = args.track
+else:
+    tracksearch = "__all__"
 if args.album:
     albumsearch = args.album
 else:
     albumsearch = "__all__"
-if args.quiet:
-    verbose = False
+if args.extrack:
+    extracksearch = args.extrack
 else:
-    verbose = True
-if args.track:
-    tracksearch = args.track
-else:
-    tracksearch = None
+    extracksearch = None
 if args.exalbum:
     exalbumsearch = args.exalbum
 else:
@@ -85,44 +79,42 @@ for (k, v) in outputs.items():
 if output_id is None:
     err = "No zone found matching " + target_zone
     sys.exit(err)
-
-# List matching albums
-albums = roonapi.list_media(output_id, ["Library", "Albums", albumsearch])
-if exalbumsearch is not None and albums:
-    albums = [chkalbum for chkalbum in albums if exalbumsearch not in chkalbum]
-if albums:
-    if "Play album" in albums:
-        albums.remove("Play album")
-    if tracksearch is not None:
-        found = None
+else:
+    album = None
+    # List matching albums
+    albums = roonapi.list_media(output_id,
+                                 ["Library",
+                                  "Albums",
+                                  albumsearch])
+    # Filter out excluded album titles
+    if exalbumsearch is not None and albums:
+        albums = [chk for chk in albums if exalbumsearch not in chk]
+    if albums:
+        track = None
         for album in albums:
             # List matching tracks
             tracks = roonapi.list_media(output_id,
-                                        ["Library", "Albums",
+                                        ["Library",
+                                         "Albums",
                                          album, tracksearch])
-            if "Play Album" in tracks:
-                tracks.remove("Play Album")
+            if extracksearch is not None and tracks:
+                tracks = [
+                    chk for chk in tracks if extracksearch not in chk]
             if tracks:
-                found = tracks[0]
-            else:
-                albums.remove(album)
-        if found is None:
+                track = tracks[0]
+                print("Playing track title", track, "on album", album)
+                roonapi.play_media(output_id, ["Library", "Albums",
+                                   album, track], None, False)
+                if len(tracks) > 1:
+                    print("\nTrack titles on", album,
+                          "matching", tracksearch, ":\n")
+                    print(*tracks, sep="\n")
+                    print("\nTo play another track on this album by title either specify")
+                    print("the full title or enough of a substring to provide a single match\n")
+                break
+            if track is not None:
+                break
+        if track is None:
             print("No tracks found matching", tracksearch)
-if albums:
-    if verbose:
-        if tracksearch is None:
-            if albumsearch == "__all__":
-                print("\nAll Albums in Library:\n")
-            else:
-                print("\nAlbums with", albumsearch, "in title", ":\n")
-        else:
-            if albumsearch == "__all__":
-                print("\nAlbums in Library containing a track matching",
-                      tracksearch, ":\n")
-            else:
-                print("\nAlbums containing a track matching", tracksearch,
-                      "with", albumsearch, "in title", ":\n")
-    print(*albums, sep="\n")
-else:
-    if verbose:
+    else:
         print("No albums found matching ", albumsearch)
